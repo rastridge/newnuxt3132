657b4312bd85b8536ebd8fe394d54f8e451ab0c2
rastridge <rfa@me.com>
2024-10-25 20:16:10 -0400

    rwc page

---
+++ pages/rwc/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>2024-10-24-Rugby World Cup in USA - Introduction</p>
+
+    <video
+      controls
+      width="100%"
+      autoplay
+    >
+      <source
+        src="https://media.buffalorugby.org/video/2024-10-24-Rugby-World-Cup-in-USA-Introduction.mov"
+        type="video/mp4"
+      />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+</template>
