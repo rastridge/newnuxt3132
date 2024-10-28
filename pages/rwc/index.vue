96ace45a1be897cf946f48502e595660899e35b9
rastridge <rfa@me.com>
2024-10-28 15:09:02 -0400

    add rwc pdf link

--- pages/rwc/index.vue
+++ pages/rwc/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <p>2024-10-24-Rugby World Cup in USA - Introduction</p>
+    <h6>
+      <nuxt-link
+        to="https://media.buffalorugby.org/xoda/files/archives/1730142008207-document-Rugby%20world%20Cup%202031%20Talking%20Points.pdf"
+        >Link to Why Buffalo should be a host city PDF</nuxt-link
+      >
+    </h6>
+    <h6>2024-10-24-Rugby World Cup in USA - Introduction Video</h6>
 
     <video
       controls
