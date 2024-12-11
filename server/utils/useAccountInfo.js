824c58a209253cfe703aa5031ee206cdb52c5ebe
rastridge <rfa@me.com>
2024-12-11 12:42:52 -0500

    clean up

--- server/utils/useAccountInfo.js
+++ server/utils/useAccountInfo.js
@@ -2,8 +2,6 @@ const { doDBQueryBuffalorugby } = useQuery()
 
 export default function useAccountInfo() {
   async function checkEmail(info) {
-    console.log('info= ', info)
-
     // check for other users with proposed email address
     let sql = `SELECT  account_email, account_id FROM inbrc_accounts WHERE deleted = 0 AND account_id <> ${info.account_id}`
     const temp = await doDBQueryBuffalorugby(sql)
@@ -12,13 +10,10 @@ export default function useAccountInfo() {
     const emailExists = temp.find((u) => {
       return u.account_email.toLowerCase() === info.account_email.toLowerCase()
     })
-    // console.log('emailexists = ', emailExists)
     return emailExists
   }
 
   async function checkEmailAdd(info) {
-    // console.log('info= ', info)
-
     // check for other users with proposed email address
     let sql = `SELECT  account_email, account_id FROM inbrc_accounts WHERE deleted = 0`
     const temp = await doDBQueryBuffalorugby(sql)
@@ -27,7 +22,6 @@ export default function useAccountInfo() {
     const emailExists = temp.find((u) => {
       return u.account_email.toLowerCase() === info.account_email.toLowerCase()
     })
-    console.log('emailexists = ', emailExists)
     return emailExists
   }
   return {
