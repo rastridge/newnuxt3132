8564994b092a2f9e9647a2cca0a815749831014b
rastridge <rfa@me.com>
2025-01-05 10:54:38 -0500

    clean up

--- pages/archives/newsletters/page/[id].vue
+++ pages/archives/newsletters/page/[id].vue
@@ -1,28 +1,29 @@
 <template>
-	<div>
-
-		<Head>
-			<Title>Buffalo Rugby Club {{ news_data.newsletter_subject }}</Title>
-		</Head>
-		<Common-header :title="news_data.newsletter_subject" />
-		<div class="my-simple-card-style">
-			<div class="my-text-style border-round-xl md:shadow-6 m-1 p-2 md:m-2 md:p-3">
-				<span v-html="news_data.newsletter_body_html"></span>
-			</div>
-		</div>
-	</div>
+  <div>
+    <Head>
+      <Title>Buffalo Rugby Club {{ data.newsletter_subject }}</Title>
+    </Head>
+    <Common-header :title="data.newsletter_subject" />
+    <div class="my-simple-card-style">
+      <div
+        class="my-text-style border-round-xl md:shadow-6 m-1 p-2 md:m-2 md:p-3"
+      >
+        <span v-html="data.newsletter_body_html"></span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-//
-// Get content id parameter
-//
-const route = useRoute()
-const news_id = ref(route.params.id)
-//
-// Get custom page content
-//
-const { data: news_data } = await useFetch(`/newsletters/${news_id.value}`, {
-	method: 'get',
-})
+  //
+  // Get content id parameter
+  //
+  const route = useRoute()
+  const id = route.params.id
+  //
+  // Get custom page content
+  //
+  const { data } = await useFetch(`/newsletters/${id}`, {
+    method: 'get',
+  })
 </script>
