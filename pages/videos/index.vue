2efa201a5e506db6ebfa51c3037ea927c3590c94
rastridge <rfa@me.com>
2025-01-05 19:35:38 -0500

    refactor - use "data"

--- pages/videos/index.vue
+++ pages/videos/index.vue
@@ -16,39 +16,45 @@
         />
       </div>
     </div>
-    <div
-      v-if="videos.length"
-      class="my-simple-card-style"
-    >
-      <ul class="list-none my-text-style">
-        <li
-          v-for="itm in filteredData"
-          :key="itm.video_id"
-          class="cursor-pointer bg-white border-round-lg border-1 p-3 m-2"
-          href="#"
-          @click.prevent="selectItem(itm.video_id)"
-        >
-          <span class="font-italic">
-            {{ $dayjs(itm.dt).format('YYYY MMM') }}
-          </span>
-          -
-          <span class="font-semibold">
-            {{ itm.video_title }}
-          </span>
-          -
-          <span class="text-lg">
-            {{ itm.video_synop }}
-          </span>
-          -
-          <span class="text-sm">
-            {{ itm.video_id }}
-          </span>
-          <span class="text-xs md:text-sm text-blue-600 cursor-pointer">
-            - Open</span
+    <div class="my-simple-card-style">
+      <div
+        v-if="filteredData.length"
+        x
+      >
+        <ul class="list-none my-text-style">
+          <li
+            v-for="itm in filteredData"
+            :key="itm.video_id"
+            class="cursor-pointer bg-white border-round-lg border-1 p-3 m-2"
+            href="#"
+            @click.prevent="selectItem(itm.video_id)"
           >
-        </li>
-      </ul>
+            <span class="font-italic">
+              {{ $dayjs(itm.dt).format('YYYY MMM') }}
+            </span>
+            -
+            <span class="font-semibold">
+              {{ itm.video_title }}
+            </span>
+            -
+            <span class="text-lg">
+              {{ itm.video_synop }}
+            </span>
+            -
+            <span class="text-sm">
+              {{ itm.video_id }}
+            </span>
+            <span class="text-xs md:text-sm text-blue-600 cursor-pointer">
+              - Open</span
+            >
+          </li>
+        </ul>
+      </div>
+      <div v-else>
+        <p class="my-text-style text-center">No videos for this year</p>
+      </div>
     </div>
+
     <!-- Modal -->
     <Dialog v-model:visible="displayModal">
       <template #header>
@@ -61,7 +67,7 @@
         <iframe
           width="560"
           height="315"
-          :src="video_url"
+          :src="item.video_url"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -87,35 +93,43 @@
   import { getIdFromURL } from 'vue-youtube-embed'
   const { $dayjs } = useNuxtApp()
 
+  //
+  // year select
   // initial values
+  //
   const year = ref(parseInt($dayjs().format('YYYY')))
   const startyear = 1985
-
-  const filteredData = computed(() => {
-    return videos.value.filter((d) => {
-      return parseInt($dayjs(d.dt).format('YYYY')) === year.value
-    })
-  })
   const onSubmit = (value) => {
     year.value = value
   }
 
+  //
+  // filter videos by year
+  //
+  const filteredData = computed(() => {
+    return data.value.filter((d) => {
+      return parseInt($dayjs(d.dt).format('YYYY')) === year.value
+    })
+  })
+
   const item = ref({})
-  const video_url = ref('')
   //
-  // Get current videos
+  // Get all current videos
   //
-  const { data: videos } = await useFetch('/videos/getallcurrent', {
+  const { data } = await useFetch('/videos/getallcurrent', {
     method: 'get',
   })
 
+  //
+  // get selected video
+  //
   const selectItem = async (id) => {
     item.value = await getOne(id)
     //
     // open modal or open window depeding on video source?
     //
     if (item.value.video_url.includes('yout')) {
-      video_url.value =
+      item.value.video_url =
         'https://www.youtube.com/embed/' + getIdFromURL(item.value.video_url)
       openModal()
     } else {
--- pages/videos/page/[id].vue
+++ pages/videos/page/[id].vue
@@ -1,13 +1,13 @@
 <template>
   <div class="topsectioncenter">
     <div class="topsectionitem">
-      <Common-header :title="video_data.video_title" />
+      <Common-header :title="data.video_title" />
     </div>
     <div class="text-center">
       <iframe
         width="568"
         height="315"
-        :src="video_url"
+        :src="data.video_url"
         title="YouTube videoplayr"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -24,20 +24,18 @@
   // Get content id parameter
   //
   const route = useRoute()
-  const video_id = ref(route.params.id)
-  const video_url = ref('')
+  const id = route.params.id
   //
   // Get video
   //
-  const { data: video_data } = await useFetch('/videos/' + video_id.value, {
+  const { data } = await useFetch('/videos/' + id, {
     method: 'get',
   })
-  video_url.value = video_data.value.video_url
 
-  if (video_url.value.includes('yout')) {
-    video_url.value =
-      'https://www.youtube.com/embed/' + getIdFromURL(video_url.value)
+  if (data.value.video_url.includes('yout')) {
+    data.value.video_url =
+      'https://www.youtube.com/embed/' + getIdFromURL(data.value.video_url)
   } else {
-    window.open(video_url.value)
+    window.open(data.value.video_url)
   }
 </script>
