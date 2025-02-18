ec7d824a330f8b2d170bfe54ce9f718474ba3bae
rastridge <rfa@me.com>
2025-02-18 16:14:43 -0500

    start newplayers

---
+++ pages/newplayers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <Title>Buffalo Rugby Club Welcomes New Players</Title>
+    </Head>
+    <div class="topsectioncenter">
+      <div class="topsectionitem">
+        <common-header title="Buffalo Rugby Club Welcomes New Players" />
+      </div>
+      <div class="topsectionitem">New players</div>
+      <img src="" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
