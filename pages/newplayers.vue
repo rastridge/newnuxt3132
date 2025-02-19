c560e550f7c831c83f7c25d51fa98b1c7fb8a90e
rastridge <rfa@me.com>
2025-02-18 21:10:10 -0500

    additional copy

--- pages/newplayers.vue
+++ pages/newplayers.vue
@@ -12,10 +12,15 @@
     <div class="card mb-1">
       <Fieldset legend="GIVE IT A TRY">
         <p class="m-0">
+          Just like any sport, rugby players come in all sizes, depending on the
+          position you play. Skills from soccer, football, basketball,
+          wrestling, all transfer well to positions in rugby. Rugby is a two way
+          sport so good tackling is essential. The one characteristic essential
+          rugby is running.<br /><br />
           Signing up is an expression of interest in being informed of the club
           activities and does not imply any commitment financially or
           otherwise.<br />
-          Just come to practice and see if we're right for you.<br />
+          Just come to practice / try out and see if we're right for you.<br />
 
           Now its time to come to a practice ( see the News page for times and
           locations)<br />
@@ -45,9 +50,13 @@
       </Fieldset>
     </div>
     <div class="card mb-1">
-      <Fieldset legend="SIGN UP">
+      <Fieldset legend="SIGN UP FOR TRYOUTS HERE">
         <div class="m-0">
-          <nuxt-link to="/register2">Register for try outs here</nuxt-link>
+          <nuxt-link
+            to="/register2"
+            class="p-3 bg-primary border-round-lg"
+            >SIGN UP</nuxt-link
+          >
         </div>
       </Fieldset>
     </div>
@@ -68,3 +77,17 @@
 <script setup>
   import team from '~/assets/imgs/carousel/team.png'
 </script>
+
+<style>
+  .my-button {
+    /* Style your button here */
+
+    background-color: blue;
+
+    padding: 10px 20px;
+
+    border: none;
+
+    cursor: pointer;
+  }
+</style>
