aedbb066319c3fc71333d469c71f1dd2b7d24b78
rastridge <rfa@me.com>
2025-02-21 15:46:46 -0500

    change sign up response message

--- pages/registered2.vue
+++ pages/registered2.vue
@@ -3,12 +3,17 @@
     <div class="card">
       <Fieldset legend="Thanks for registering">
         <p class="m-0">
-          Now its time to come to a practice ( see the News page for times and
-          locations) <br />
-
+          Now its time to come to a practice ( see Events page for times and
+          locations) <br /><br />
+          <nuxt-link
+            to="https://buffalorugby.org/events"
+            class="p-3 bg-primary border-round-lg"
+            target="blank"
+            >Events</nuxt-link
+          ><br /><br />
           Wear shorts, cleats and a t-shirt ( or warmer if necessary)<br />
 
-          When youu arrive, ask someone to point out a coach, introduce
+          When you arrive, ask someone to point out a coach, introduce
           yourself.<br />
         </p>
       </Fieldset>
