01bf15354ce1889e55c8d79cffbca99f4be0d51a
rastridge <rfa@me.com>
2025-05-26 10:30:44 -0400

    change to nuxt3 api

--- composables/useFetchAll.js
+++ composables/useFetchAll.js
@@ -1,41 +1,47 @@
 import { useAuthStore } from '~/stores/authStore'
 
 export default function useFetchAll() {
-	const auth = useAuthStore()
+  const auth = useAuthStore()
 
-	const getAll = async (app) => {
-		const { data } = await useFetch(`/${app}/getall`, {
-			method: 'get',
-			headers: {
-				authorization: auth.user.token,
-			},
-		})
+  const getAll = async (app) => {
+    const { data } = await useFetch(
+      `https://nuxt3.buffalorugby.org/${app}/getall`,
+      {
+        method: 'get',
+        headers: {
+          authorization: auth.user.token,
+        },
+      },
+    )
 
-		return { data }
-	}
+    return { data }
+  }
 
-	const deleteOne = async (app, id) => {
-		const { data } = await useFetch(`/${app}/${id}`, {
-			method: 'DELETE',
-			headers: {
-				authorization: auth.user.token,
-			},
-		})
-		return data
-	}
+  const deleteOne = async (app, id) => {
+    const { data } = await useFetch(
+      `https://nuxt3.buffalorugby.org/${app}/${id}`,
+      {
+        method: 'DELETE',
+        headers: {
+          authorization: auth.user.token,
+        },
+      },
+    )
+    return data
+  }
 
-	const changeStatusOne = async (app, { id, status }) => {
-		await useFetch(`/${app}/status`, {
-			method: 'POST',
-			headers: {
-				authorization: auth.user.token,
-			},
-			body: { id, status },
-		})
-	}
-	return {
-		getAll,
-		deleteOne,
-		changeStatusOne,
-	}
+  const changeStatusOne = async (app, { id, status }) => {
+    await useFetch(`https://nuxt3.buffalorugby.org/${app}/status`, {
+      method: 'POST',
+      headers: {
+        authorization: auth.user.token,
+      },
+      body: { id, status },
+    })
+  }
+  return {
+    getAll,
+    deleteOne,
+    changeStatusOne,
+  }
 }
--- stores/authStore.js
+++ stores/authStore.js
@@ -2,67 +2,54 @@
 import { useAlertStore } from '~/stores/alertStore'
 
 export const useAuthStore = defineStore('auth', {
-	state: () => ({
-		status: { loggedIn: false },
-		user: {},
-		// keep: { keeped: false },
-	}),
-	getters: {
-		loggingIn: (state) => state.status.loggingIn,
-		isLoggedIn: (state) => state.status.loggedIn,
-		// isKeeped: (state) => state.keep.keeped,
-		getUser: (state) => state.status.user,
-	},
+  state: () => ({
+    status: { loggedIn: false },
+    user: {},
+    // keep: { keeped: false },
+  }),
+  getters: {
+    loggingIn: (state) => state.status.loggingIn,
+    isLoggedIn: (state) => state.status.loggedIn,
+    // isKeeped: (state) => state.keep.keeped,
+    getUser: (state) => state.status.user,
+  },
 
-	actions: {
-		navigate(p) {
-			// const router = useRouter()
-			return navigateTo({
-				path: p,
-			})
-		},
+  actions: {
+    navigate(p) {
+      // const router = useRouter()
+      return navigateTo({
+        path: p,
+      })
+    },
 
-		/* 		async login(username, password, keeploggedin) {
+    /* 		async login(username, password, keeploggedin) {
 			this.loginRequest(username, password, keeploggedin)
 		},
  */
-		async login(username, password) {
-			this.loginRequest(username, password)
-		},
+    async login(username, password) {
+      this.loginRequest(username, password)
+    },
 
-		/* 		async loginRequest(username, password, keeploggedin) {
-			const alert = useAlertStore()
-			this.status = { loggedIn: false }
-			alert.attempt('Logging in . . .')
-			const user = await $fetch('/users/authenticate', {
-				method: 'POST',
-				body: { username, password },
-			})
-			if (user.match) {
-				this.user = user
-				this.loginSuccess(user, keeploggedin)
-			} else {
-				this.loginFailure()
-			}
-		}, */
+    async loginRequest(username, password) {
+      const alert = useAlertStore()
+      this.status = { loggedIn: false }
+      alert.attempt('Logging in . . .')
+      const user = await $fetch(
+        'https://nuxt3.buffalorugby.org/users/authenticate',
+        {
+          method: 'POST',
+          body: { username, password },
+        },
+      )
+      if (user.match) {
+        this.user = user
+        this.loginSuccess(user)
+      } else {
+        this.loginFailure()
+      }
+    },
 
-		async loginRequest(username, password) {
-			const alert = useAlertStore()
-			this.status = { loggedIn: false }
-			alert.attempt('Logging in . . .')
-			const user = await $fetch('/users/authenticate', {
-				method: 'POST',
-				body: { username, password },
-			})
-			if (user.match) {
-				this.user = user
-				this.loginSuccess(user)
-			} else {
-				this.loginFailure()
-			}
-		},
-
-		/* loginSuccess(user, keeploggedin) {
+    /* loginSuccess(user, keeploggedin) {
 			this.status = { loggedIn: true }
 			this.user = user
 			this.keep = { keeped: keeploggedin }
@@ -77,18 +64,18 @@ export const useAuthStore = defineStore('auth', {
 			navigateTo('/admin')
 		}, */
 
-		loginSuccess(user) {
-			this.status = { loggedIn: true }
-			this.user = user
-			sessionStorage.removeItem('auth')
-			sessionStorage.setItem('auth', JSON.stringify(this.user))
+    loginSuccess(user) {
+      this.status = { loggedIn: true }
+      this.user = user
+      sessionStorage.removeItem('auth')
+      sessionStorage.setItem('auth', JSON.stringify(this.user))
 
-			const alert = useAlertStore()
-			alert.success('Login successful')
-			navigateTo('/admin')
-		},
+      const alert = useAlertStore()
+      alert.success('Login successful')
+      navigateTo('/admin')
+    },
 
-		/* 		logout() {
+    /* 		logout() {
 			const alert = useAlertStore()
 			alert.clear()
 			this.status = { loggedIn: false }
@@ -99,17 +86,17 @@ export const useAuthStore = defineStore('auth', {
 			navigateTo('/')
 		}, */
 
-		logout() {
-			const alert = useAlertStore()
-			alert.clear()
-			this.status = { loggedIn: false }
-			this.user = {}
-			// localStorage.removeItem('auth')
-			sessionStorage.removeItem('auth')
-			navigateTo('/')
-		},
+    logout() {
+      const alert = useAlertStore()
+      alert.clear()
+      this.status = { loggedIn: false }
+      this.user = {}
+      // localStorage.removeItem('auth')
+      sessionStorage.removeItem('auth')
+      navigateTo('/')
+    },
 
-		/* 		loginFailure() {
+    /* 		loginFailure() {
 			this.status = { loggedIn: false }
 			this.keep = { keeped: false }
 			this.user = {}
@@ -118,20 +105,20 @@ export const useAuthStore = defineStore('auth', {
 			navigateTo('/loginpage')
 		}, */
 
-		loginFailure() {
-			this.status = { loggedIn: false }
-			this.user = {}
-			const alert = useAlertStore()
-			alert.error('Login failed - try again')
-			navigateTo('/loginpage')
-		},
+    loginFailure() {
+      this.status = { loggedIn: false }
+      this.user = {}
+      const alert = useAlertStore()
+      alert.error('Login failed - try again')
+      navigateTo('/loginpage')
+    },
 
-		/* 		loginAuto() {
+    /* 		loginAuto() {
 			this.user = JSON.parse(localStorage.getItem('auth'))
 			this.status = { loggedIn: true }
 			this.keep = { keeped: true }
 			sessionStorage.setItem('auth', JSON.stringify(this.user))
 			navigateTo('/admin')
 		}, */
-	},
+  },
 })
